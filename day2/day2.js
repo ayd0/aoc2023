d55ebe826e5df940d07bc9a6732fc820e921b005
ayd0 <austindaytonogle@gmail.com>
2023-12-01 23:15:16 -0800

    day 2, part 2

--- day2/day2.js
+++ day2/day2.js
@@ -11,17 +11,38 @@ const limits = {
 
 let total = 0;
 
-data.forEach((game, idx) => {
-    let pass = true;
-    game.slice(game.indexOf(':') + 2).split(';').some(round => {
-        round.split(',').some(handful => {
-            handful = handful.trim().split(' ');
-            if (limits[handful[1]] < handful[0]) {
-                pass = false;
-            }
-        })
-    });
-    if (pass) total += idx + 1;
-})
+if (process.argv[3] === 1) {
+    // day 1
+    data.forEach((game, idx) => {
+        let pass = true;
+        game.slice(game.indexOf(':') + 2).split(';').some(round => {
+            round.split(',').some(handful => {
+                handful = handful.trim().split(' ');
+                if (limits[handful[1]] < handful[0]) {
+                    pass = false;
+                }
+            })
+        });
+        if (pass) total += idx + 1;
+    })
+} else {
+    // day 2
+    data.forEach((game, idx) => {
+        const reqColors = {
+            "red": 0,
+            "green": 0,
+            "blue": 0
+        }
+        game.slice(game.indexOf(':') + 2).split(';').some(round => {
+            round.split(',').some(handful => {
+                handful = handful.trim().split(' ');
+                if (+reqColors[handful[1]] < +handful[0]) {
+                    reqColors[handful[1]] = +handful[0];
+                }
+            })
+        });
+        total += reqColors.red * reqColors.green * reqColors.blue;
+    })
+}
 
 console.log(total);
